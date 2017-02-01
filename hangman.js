030d99b641381d05578ef7aa258d03c0ac739350
Mike <mahathaway93@gmail.com>
2017-02-01 11:35:45 -0500

    upgraded game logic

--- hangman.js
+++ hangman.js
@@ -1,7 +1,6 @@
 window.alert('We\'re in Business?');
 
 //My implementation of the game of hangman.
-//I have attempted to implement this in the functional style
 
 //Global data structures
 let incorrectLetters = [];
@@ -27,17 +26,13 @@ const checkGuess = function(){
 	else if(getGuess === pickWord){
 		return showAnswerAndCongragulatePlayer();
 	}
-	//need to resolve the delay between correctLetters pushing and analyzing
-	else if(correctLetters.join('') === pickWord){
+	else if(correctLetters.join('') + getGuess === pickWord){
 		return showAnswerAndCongragulatePlayer();
 	}
-	console.log(getGuess,correctLetters,incorrectLetters);
 	return updateGameState(getGuess,true);
 }
 
-//Added sideeffects :(
-	//Will review upon completion of prototype
-	//May be able to simply invoke the showPlayerProgress function
+
 const updateGameState = function(letter,truthiness){
 	if(truthiness === false){
 		return incorrectLetters.push(letter);
@@ -45,12 +40,9 @@ const updateGameState = function(letter,truthiness){
 	else if(truthiness === true){
 		return correctLetters.push(letter);
 	}
-	console.log(correctLetters,incorrectLetters);
 	return showPlayerProgress(incorrectLetters,correctLetters);
 }
 
-//Need to check why prompts are being continually generated
-	//answer may lie in the usage of the bind method - need to investigate further
 function startGame(){
 	//use incorrectLetters.length to keep track of the number of guesses made
 	while(guessed === false && incorrectLetters.length < 6){
@@ -59,17 +51,6 @@ function startGame(){
 	if(incorrectLetters.length === 6){
 		return gameOver();
 	}
-}
-
-//draw on canvas? Alternative would be to have a global string structure
-const showPlayerProgress = function(incorrectLetters,correctLetters){
-	//canvas setup
-	let canvas = document.getElementById('canvas');
-	let context = canvas.getContext('2d');
-
-	context.fillText(incorrectLetters.join(' '),50,50);
-	context.fillText(correctLetters.join(' '),100,100);
-
 }
 
 const showAnswerAndCongragulatePlayer = function(){
@@ -81,5 +62,26 @@ const gameOver = function(){
 	return window.alert('SAD!')
 }
 
+//Draw latest gamestate on canvas
+const showPlayerProgress = function(incorrectLetters,correctLetters){
+	//canvas setup
+	let canvas = document.getElementById('canvas');
+	let context = canvas.getContext('2d');
 
+	context.fillStyle = 'Black';
+
+	context.fillText(incorrectLetters.join(''),50,50);
+	context.fillText(correctLetters.join(''),100,100);
+
+}
+
+//clears the canvas so latest game state can be drawn
+const clearCanvas = function(){
+	let canvas = document.getElementById('canvas');
+	let context = canvas.getContext('2d');
+
+	return context.clearRect(0,0,canvas.width,canvas.height);
+}
+
+//function call that starts the game
 startGame();
